2d977f20a1761e62908d6dfd46cc46fe121bbec0
Julian Schmuckli <julian@schmuckli.net>
2019-04-16 23:53:22 +0200

    Fixed bug of counting products

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
     <v-layout row wrap>
-      <v-flex lg3 md4 sm6 xs12 v-for="(list, index) in lists" :key="list.id">
-        <List :list="list" :amount_items="lists_meta[index]" />
+      <v-flex lg3 md4 sm6 xs12 v-for="(list) in lists" :key="list.id">
+        <List :list="list" :amount_items="lists_meta[list.id]" />
       </v-flex>
     </v-layout>
   </v-container>
@@ -37,13 +37,14 @@ export default {
         lists.forEach(function(list){
           db.collection("users").doc(firebase.auth().currentUser.uid).collection("lists").doc(list.id).collection("items")
           .onSnapshot(function(items){
+            var current_list_id = items.query._query.path.segments[3];
             var not_purchased_items_counter = 0;
             items.forEach(function(item){
               if(!item.data().purchased){
                 not_purchased_items_counter++;
               }
             });
-            global_this.lists_meta.push(not_purchased_items_counter);
+            global_this.$set(global_this.lists_meta, current_list_id, not_purchased_items_counter); //Equal to global_this.lists_meta[key] = value
           });
           global_this.lists.push(Object.assign({id: list.id}, list.data()));
         });
