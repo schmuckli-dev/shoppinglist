7d46c155327d09487000270a95189fa2c7078650
Julian Schmuckli <julian@schmuckli.net>
2019-04-17 22:29:24 +0200

    Enabled offline persistance

--- src/main.js
+++ src/main.js
@@ -20,6 +20,20 @@ firebase.initializeApp({
   projectId: "shoppinglist-schmuckli"
 });
 
+//Offline Persistence for FireStore
+firebase.firestore().enablePersistence()
+.catch(function(err) {
+    if (err.code == 'failed-precondition') {
+        // Multiple tabs open, persistence can only be enabled
+        // in one tab at a a time.
+        // ...
+    } else if (err.code == 'unimplemented') {
+        // The current browser does not support all of the
+        // features required to enable persistence
+        // ...
+    }
+});
+
 const i18n = new VueI18n({
   locale: getLanguage(), // set locale
   fallbackLocale: 'en',
