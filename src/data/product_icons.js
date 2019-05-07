83bb347b988872aad76879d154944ae69a2c4547
Julian Schmuckli <julian@schmuckli.net>
2019-05-08 00:40:17 +0200

    Added more products

--- src/data/product_icons.js
+++ src/data/product_icons.js
@@ -8,6 +8,40 @@ export default {
         "äpfel",
         "apples"
       ]
+    },
+    {
+      icon: "cheese-swiss",
+      names: [
+        "käse",
+        "emmentaler",
+        "käseplatte",
+        "cheese",
+        "käsefondue"
+      ]
+    },
+    {
+      icon: "corn",
+      names: [
+        "mais",
+        "corn",
+        "maiskolben"
+      ]
+    },
+    {
+      icon: "cookie",
+      names: [
+        "cookie",
+        "cookies",
+        "kekse"
+      ]
+    },
+    {
+      icon: "egg",
+      names: [
+        "egg",
+        "ei",
+        "eier"
+      ]
     }
   ]
 }
