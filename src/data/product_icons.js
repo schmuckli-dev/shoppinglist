56d3e08c9328f9a299b2155a553a67d589dfa620
Julian Schmuckli <julian@schmuckli.net>
2020-01-11 18:10:18 +0100

    Added again some new products

--- src/data/product_icons.js
+++ src/data/product_icons.js
@@ -1,329 +1,218 @@
 export default {
-  products: [
-    {
-      icon: "apple-alt",
-      names: [
-        "apfel",
-        "apple",
-        "äpfel",
-        "apples"
-      ]
-    },
-    {
-      icon: "cheese-swiss",
-      names: [
-        "käse",
-        "emmentaler",
-        "käseplatte",
-        "cheese",
-        "käsefondue"
-      ]
-    },
-    {
-      icon: "corn",
-      names: [
-        "mais",
-        "corn",
-        "maiskolben"
-      ]
-    },
-    {
-      icon: "cookie",
-      names: [
-        "cookie",
-        "cookies",
-        "kekse"
-      ]
-    },
-    {
-      icon: "popcorn",
-      names: [
-        "popcorn",
-        "popcorn"
-      ]
-    },
-    {
-      icon: "bread-loaf",
-      names: [
-        "bread",
-        "breads",
-        "brot",
-        "brote",
-        "toast",
-        "toasts"
-      ]
-    },
-    {
-      icon: "fish",
-      names: [
-        "fish",
-        "fisch",
-        "lachs",
-        "salmon",
-        "forelle"
-      ]
-    },
-    {
-      icon: "carrot",
-      names: [
-        "carrot",
-        "carrots",
-        "karotte",
-        "karotten",
-        "rüebli"
-      ]
-    },
-    {
-      icon: "pizza",
-      names: [
-        "pizza",
-        "pizzas"
-      ]
-    },
-    {
-      icon: "pumpkin",
-      names: [
-        "pumpkin",
-        "pumpkins",
-        "kürbis",
-        "kürbisse"
-      ]
-    },
-    {
-      icon: "drumstick",
-      names: [
-        "drumstick",
-        "poulet",
-        "poulet leg",
-        "poulet bein"
-      ]
-    },
-    {
-      icon: "steak",
-      names: [
-        "steak",
-        "fleisch",
-        "bbq",
-        "filet"
-      ]
-    },
-    {
-      icon: "ice-cream",
-      names: [
-        "eis",
-        "eiscreme",
-        "eis creme",
-        "ice cream",
-        "ice",
-        "icecream"
-      ]
-    },
-    {
-      icon: "lemon",
-      names: [
-        "lemon",
-        "zitrone",
-        "zitron",
-        "citron",
-        "zitronenschnitz"
-      ]
-    },
-    {
-      icon: "hotdog",
-      names: [
-        "hotdog",
-        "hotdogs",
-        "hot dog"
-      ]
-    },
-    {
-      icon: "french-fries",
-      names: [
-        "french fries",
-        "pommes",
-        "pommes frites",
-        "frenchfries"
-      ]
-    },
-    {
-      icon: "sandwich",
-      names: [
-        "sandwich",
-        "sandwiches"
-      ]
-    },
-    {
-      icon: "hamburger",
-      names: [
-        "hamburger",
-        "ham burger"
-      ]
-    },
-    {
-      icon: "cheeseburger",
-      names: [
-        "cheeseburger",
-        "cheese burger"
-      ]
-    },
-    {
-      icon: "wine-bottle",
-      names: [
-        "wine",
-        "wein",
-        "weine",
-        "flasche",
-        "bottle",
-        "mineral",
-        "mineralwasser",
-        "coca cola",
-        "coke",
-        "eistee",
-        "ice tea",
-        "iceatea",
-        "rivella",
-        "sprite",
-        "7up",
-        "7 up"
-      ]
-    },
-    {
-      icon: "coffee",
-      names: [
-        "coffee",
-        "kaffee",
-        "eiskaffee",
-        "ice coffee",
-        "icecoffee",
-        "tee",
-        "tea"
-      ]
-    },
-    {
-      icon: "beer",
-      names: [
-        "beer",
-        "bier",
-        "biere"
-      ]
-    },
-    {
-      icon: "salad",
-      names: [
-        "salad",
-        "salat",
-        "eisberg"
-      ]
-    },
-    {
-      icon: "egg",
-      names: [
-        "egg",
-        "eggs",
-        "ei",
-        "eier"
-      ]
-    },
-    {
-      icon: "lightbulb",
-      names: [
-        "bulb",
-        "lightbulb",
-        "lampe",
-        "glühbirne",
-        "led",
-        "e27",
-        "e14",
-        "gu10",
-        "halogen"
-      ]
-    },
-    {
-      icon: "socks",
-      names: [
-        "socks",
-        "sock",
-        "socken",
-        "socke",
-        "strumpf",
-        "strümpfe"
-      ]
-    },
-    {
-      icon: "toilet-paper",
-      names: [
-        "toiletpaper",
-        "toiletpapers",
-        "toilettenpapier",
-        "toilettepapier"
-      ]
-    },
-    {
-      icon: "toothbrush",
-      names: [
-        "zahnbürste",
-        "toothbrush",
-        "zahnbürsten",
-        "toothbrushes",
-        "tooth brushes"
-      ]
-    },
-    {
-      icon: "tshirt",
-      names: [
-        "tshirt",
-        "t-shirt",
-        "kleidung",
-        "clothes",
-        "kleider"
-      ]
-    },
-    {
-      icon: "watch",
-      names: [
-        "clock",
-        "uhr",
-        "armbanduhr",
-        "wanduhr",
-        "wristband",
-        "watch",
-        "rolex",
-        "fitbit"
-      ]
-    },
-    {
-      icon: "duck",
-      names: [
-        "duck",
-        "ente",
-        "enten",
-        "ducks"
-      ]
-    },
-    {
-      icon: "pie",
-      names: [
-        "pie",
-        "pies",
-        "kuchen",
-        "torte",
-        "torten"
-      ]
-    },
-    {
-      icon: "envelope",
-      names: [
-        "brief",
-        "couvert",
-        "envelope"
-      ] 
-    }, 
-    {
-      icon: "pills",
-      names: [
-        "tabletten",
-        "tabl."
-      ]
-   } 
-  ]
-}
+    products: [
+        {
+            icon: "apple-alt",
+            names: ["apfel", "apple", "äpfel", "apples"]
+        },
+        {
+            icon: "cheese-swiss",
+            names: ["käse", "emmentaler", "käseplatte", "cheese", "käsefondue"]
+        },
+        {
+            icon: "corn",
+            names: ["mais", "corn", "maiskolben"]
+        },
+        {
+            icon: "cookie",
+            names: ["cookie", "cookies", "kekse"]
+        },
+        {
+            icon: "popcorn",
+            names: ["popcorn", "popcorn"]
+        },
+        {
+            icon: "bread-loaf",
+            names: ["bread", "breads", "brot", "brote", "toast", "toasts"]
+        },
+        {
+            icon: "fish",
+            names: ["fish", "fisch", "lachs", "salmon", "forelle"]
+        },
+        {
+            icon: "carrot",
+            names: ["carrot", "carrots", "karotte", "karotten", "rüebli"]
+        },
+        {
+            icon: "pizza",
+            names: ["pizza", "pizzas"]
+        },
+        {
+            icon: "pumpkin",
+            names: ["pumpkin", "pumpkins", "kürbis", "kürbisse"]
+        },
+        {
+            icon: "drumstick",
+            names: ["drumstick", "poulet", "poulet leg", "poulet bein"]
+        },
+        {
+            icon: "steak",
+            names: ["steak", "fleisch", "bbq", "filet"]
+        },
+        {
+            icon: "ice-cream",
+            names: [
+                "eis",
+                "eiscreme",
+                "eis creme",
+                "ice cream",
+                "ice",
+                "icecream"
+            ]
+        },
+        {
+            icon: "lemon",
+            names: ["lemon", "zitrone", "zitron", "citron", "zitronenschnitz"]
+        },
+        {
+            icon: "hotdog",
+            names: ["hotdog", "hotdogs", "hot dog"]
+        },
+        {
+            icon: "french-fries",
+            names: ["french fries", "pommes", "pommes frites", "frenchfries"]
+        },
+        {
+            icon: "sandwich",
+            names: ["sandwich", "sandwiches"]
+        },
+        {
+            icon: "hamburger",
+            names: ["hamburger", "ham burger"]
+        },
+        {
+            icon: "cheeseburger",
+            names: ["cheeseburger", "cheese burger"]
+        },
+        {
+            icon: "wine-bottle",
+            names: [
+                "wine",
+                "wein",
+                "weine",
+                "flasche",
+                "bottle",
+                "mineral",
+                "mineralwasser",
+                "coca cola",
+                "coke",
+                "eistee",
+                "ice tea",
+                "iceatea",
+                "rivella",
+                "sprite",
+                "7up",
+                "7 up"
+            ]
+        },
+        {
+            icon: "coffee",
+            names: [
+                "coffee",
+                "kaffee",
+                "eiskaffee",
+                "ice coffee",
+                "icecoffee",
+                "tee",
+                "tea"
+            ]
+        },
+        {
+            icon: "beer",
+            names: ["beer", "bier", "biere"]
+        },
+        {
+            icon: "salad",
+            names: ["salad", "salat", "eisberg"]
+        },
+        {
+            icon: "egg",
+            names: ["egg", "eggs", "ei", "eier"]
+        },
+        {
+            icon: "lightbulb",
+            names: [
+                "bulb",
+                "lightbulb",
+                "lampe",
+                "glühbirne",
+                "led",
+                "e27",
+                "e14",
+                "gu10",
+                "halogen"
+            ]
+        },
+        {
+            icon: "socks",
+            names: ["socks", "sock", "socken", "socke", "strumpf", "strümpfe"]
+        },
+        {
+            icon: "toilet-paper",
+            names: [
+                "toiletpaper",
+                "toiletpapers",
+                "toilettenpapier",
+                "toilettepapier"
+            ]
+        },
+        {
+            icon: "toothbrush",
+            names: [
+                "zahnbürste",
+                "toothbrush",
+                "zahnbürsten",
+                "toothbrushes",
+                "tooth brushes"
+            ]
+        },
+        {
+            icon: "tshirt",
+            names: ["tshirt", "t-shirt", "kleidung", "clothes", "kleider"]
+        },
+        {
+            icon: "watch",
+            names: [
+                "clock",
+                "uhr",
+                "armbanduhr",
+                "wanduhr",
+                "wristband",
+                "watch",
+                "rolex",
+                "fitbit"
+            ]
+        },
+        {
+            icon: "duck",
+            names: ["duck", "ente", "enten", "ducks"]
+        },
+        {
+            icon: "pie",
+            names: ["pie", "pies", "kuchen", "torte", "torten"]
+        },
+        {
+            icon: "envelope",
+            names: ["brief", "couvert", "envelope"]
+        },
+        {
+            icon: "pills",
+            names: ["tabletten", "tabl."]
+        },
+        {
+          icon: "lock",
+          names: ["schloss", "schlösser", "lock", "locker"]
+        },
+        {
+          icon: "lock",
+          names: ["schloss", "schlösser", "lock", "locker"]
+        },
+        {
+          icon: "croissant",
+          names: ["croissant", "gipfel", "gipfel"]
+        },
+        {
+          icon: "fan",
+          names: ["lüfter", "dyson", "fan"]
+        }
+    ]
+};
